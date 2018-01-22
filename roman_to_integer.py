d746982c43729d92ef94708e793f61ca2f85a433
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-01-22 23:00:43 +0800

    Roman to Integer

---
+++ roman_to_integer.py
@@ -0,0 +1,42 @@
+"""
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+
+
+"""
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        sum = 0
+        dic = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        for x in range (len(s)-1,-1,-1):
+            if x == len(s)-1:
+                sum = sum + dic [s[x]]
+            elif x > 0:
+                if dic[s[x]] < dic[s[x+1]]:
+                    sum = sum - dic[s[x]]
+                else:
+                    sum = sum + dic[s[x]]
+            elif x == 0:
+                if dic[s[x]] >= dic[s[x+1]]:
+                    sum = sum + dic[s[x]]
+                else:
+                    sum = sum - dic[s[x]]
+        return sum
+
+
+
+if __name__ =='__main__':
+    sol = Solution()
+    print sol.romanToInt('III')
+    print sol.romanToInt('XII')
+    print sol.romanToInt('XCIX')
+    print sol.romanToInt('MDCCC')
+    print sol.romanToInt('MMMCCCXXXIII')
+    print sol.romanToInt('D')
+    print sol.romanToInt('MCMXCVI')
