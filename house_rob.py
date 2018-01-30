da990b7492a62fa2fb85d8c0b86bff6d36556913
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-01-30 18:39:52 +0800

    House Robber

---
+++ house_rob.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        elif len(nums) == 1:
+            return nums[0]
+
+        result = [nums[0]]
+        result.append(max(nums[0], nums[1]))
+        value = 0
+        for i in range(2, len(nums)):
+            value = max(result[i - 2] + nums[i], result[i - 1])
+            result.append(value)
+
+        return value
