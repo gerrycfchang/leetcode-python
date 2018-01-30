dca47d19ed2e60e3aca2d576f1ae03e99bc283f6
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-01-30 15:08:12 +0800

    bit problems

---
+++ bit/reverse_bit.py
@@ -0,0 +1,20 @@
+"""
+Reverse bits of a given 32 bits unsigned integer.
+
+For example, given input 43261596 (represented in binary as 00000010100101000001111010011100),
+return 964176192 (represented in binary as 00111001011110000010100101000000).
+
+"""
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        res = 0
+        for i in range(32):
+            if n & 1 == 1:
+                res = (res << 1) + 1
+            else:
+                res = res << 1
+            n >>= 1
+
+        return res
