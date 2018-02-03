2eac90c59cb9609b5fd6adaf13bc4e609ab564f4
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-02-03 22:48:00 +0800

    Linked List Cycle

---
+++ linkedlist/linked_list_cycle.py
@@ -0,0 +1,27 @@
+"""
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+
+"""
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head: return False
+        slow = fast = head
+
+        isCycle = False
+
+        while fast.next is not None and fast.next.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                isCycle = True
+                break
+        return isCycle
---
+++ linkedlist/test_linkedListCycle.py
@@ -0,0 +1,43 @@
+from unittest import TestCase
+import sys
+sys.path.append('../')
+from leetCodeUtil import LinkedList
+from leetCodeUtil import ListNode
+import unittest
+from linked_list_cycle import Solution
+
+class TestSolution(TestCase):
+    def test_linkedListCycleCase1(self):
+        sol = Solution()
+
+        l1 = LinkedList()
+        l1.insertNode(1)
+
+        self.assertEqual(sol.hasCycle(l1.head), False)
+
+
+    def test_linkedListCycleCase2(self):
+        sol = Solution()
+
+        """
+        1 > 2 > 3 > 4 > 5 > 6 > 5
+
+        """
+
+        node1 = ListNode(1)
+        node2 = ListNode(2)
+        node3 = ListNode(3)
+        node4 = ListNode(4)
+        node5 = ListNode(5)
+        node6 = ListNode(6)
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
+        node4.next = node5
+        node5.next = node6
+        node6.next = node5
+
+        self.assertEqual(sol.hasCycle(node1), True)
+
+if __name__ == '__main__':
+    unittest.main()
