aec6ad6ef7aab950ca35ccb951801a0d2fe7e72e
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-02-07 22:29:32 +0800

    374. Guess Number Higher or Lower

---
+++ google/guess_number_higher_lower.py
@@ -0,0 +1,40 @@
+"""
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I'll tell you whether the number is higher or lower.
+
+You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+
+-1 : My number is lower
+ 1 : My number is higher
+ 0 : Congrats! You got it!
+
+"""
+
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        MIN = 1
+        MAX = n
+
+        while MIN <= MAX:
+            g = (MAX + MIN) / 2
+            result = guess(g)
+            if result == 0:
+                return g
+            elif result == 1:
+                MIN = g + 1
+            elif result == -1:
+                MAX = g - 1
+
