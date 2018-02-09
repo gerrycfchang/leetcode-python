30d0114069cb98a0a466b98933b4bbb888bc7a3b
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-02-09 10:11:34 +0800

    289. Game of Life

---
+++ google/medium/game_of_life.py
@@ -0,0 +1,36 @@
+"""
+
+According to the Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+
+Given a board with m by n cells, each cell has an initial state live (1) or dead (0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+
+Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+Any live cell with two or three live neighbors lives on to the next generation.
+Any live cell with more than three live neighbors dies, as if by over-population..
+Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+Write a function to compute the next state (after one update) of the board given its current state.
+
+"""
+
+class Solution(object):
+    def gameOfLife(self, board):
+        """
+        :type board: List[List[int]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+        m, n = len(board), len(board[0])
+
+        for i in range(m):
+            for j in range(n):
+                # calculate the number of lives
+                live = 0
+                for I in range(max(i-1, 0), min(i+2, m)):
+                    for J in range(max(j-1,0), min(j+2, n)):
+                        live += board[I][J] & 1
+
+                if (live == 4 and board[i][j]) or live == 3:
+                    board[i][j] |= 2
+
+        for i in range(m):
+            for j in range(n):
+                board[i][j] >>= 1
