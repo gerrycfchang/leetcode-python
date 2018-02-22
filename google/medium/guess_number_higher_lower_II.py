403349a958c4c3b799dc03f23a90634490da36ce
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-02-22 18:01:28 +0800

    Refactor 375.

--- google/medium/guess_number_higher_lower_II.py
+++ google/medium/guess_number_higher_lower_II.py
@@ -19,7 +19,7 @@ class Solution(object):
     def loopSolve(self, dp, n):
         for i in range(2, n + 1):
             for j in range(i - 1, -1, -1):
-                global_min = 9999
+                global_min = float('inf')
                 for k in range(j + 1, i):
                     local_max = k + max(dp[j][k - 1], dp[k + 1][i])
                     global_min = min(local_max, global_min)
@@ -32,14 +32,10 @@ class Solution(object):
     def solve(self, dp, L, R):
         if L >= R: return 0
         if dp[L][R]: return dp[L][R]
-
-        minCost = L + max(self.solve(dp, L, L-1), self.solve(dp, L + 1, R))
-        for k in range(L+1, R + 1):
-            temp = k + max(self.solve(dp, L, k - 1), self.solve(dp, k + 1, R))
-            if temp < minCost:
-                minCost = temp
-        dp[L][R] = minCost
-
+        dp[L][R] = float('inf')
+        for i in range(L, R+1):
+            dp[L][R] = min(dp[L][R], i + max(self.solve(dp,L,i-1),self.solve(dp,i+1,R)))
+        
         #dp[L][R] = min(k + max(self.solve(dp, L, k - 1), self.solve(dp, k + 1, R)) for k in range(L, R + 1))
         return dp[L][R]
 
@@ -53,3 +49,7 @@ class Solution(object):
                 else:
                     self.print_path(dp, i + 1, R)
                 break
+
+if __name__ == '__main__':
+    sol = Solution()
+    assert (sol.getMoneyAmount(4) == 4)
