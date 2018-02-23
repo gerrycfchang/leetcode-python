9ff2e908d8f4220036e9890c8acdd16abbfc429c
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-02-23 10:55:12 +0800

    Add 279. another solution

--- dp/perfect_sqaures.py
+++ dp/perfect_sqaures.py
@@ -9,6 +9,30 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        ### Solution 1
+        import math
+        if n == 0: return 0
+        dp = [float('inf') for _ in range(n+1)]
+        dp[0] = 0
+        squares = [i ** 2 for i in range(1, int(math.sqrt(n)+1))]
+        for i in range (1, n+1):
+            for j in range(len(squares)):
+                sq = squares[j]
+                if sq == i:
+                    dp[i] = 1
+                elif sq <= i:
+                    dp[i] = min(dp[i], dp[sq]+dp[i-sq])
+                else:
+                    break
+        return dp[n]
+
+    ### Solution 2
+    def numSquaresSol(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ### Solution 1
         import math
         if n == 0: return 0
         dp = [float('inf') for _ in range(n+1)]
@@ -17,10 +41,12 @@ class Solution(object):
         for i in range (1, n+1):
             for j in range(len(squares)):
                 if squares[j] <= i:
-                    dp[i] = min(dp[i], dp[i-squares[j]] + 1)
+                    dp[i] = min(dp[i], dp[i-squares[j]]+1)
         return dp[n]
 
 if __name__ == '__main__':
     sol = Solution()
     assert sol.numSquares(12) == 3
+    assert sol.numSquaresSol(12) == 3
     assert sol.numSquares(13) == 2
+    assert sol.numSquares(6922) == 2
