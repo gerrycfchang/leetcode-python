6f3aa4e57bb08acd296e1a5d9525ecc566b45fd4
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-03-07 23:05:11 +0800

    Add 516. Longest Palindromic Subsequence from left DP

--- longest/longest_palindromic_subsequence.py
+++ longest/longest_palindromic_subsequence.py
@@ -20,7 +20,7 @@
 
 
 class Solution(object):
-    def longestPalindromeSubseq(self, s):
+    def longestPalindromeSubseqFromRight(self, s):
         """
         :type s: str
         :rtype: int
@@ -36,10 +36,29 @@ class Solution(object):
                 else:
                     dp[i][j] = max(dp[i+1][j], dp[i][j-1])
         return dp[0][n-1]
+    
+    def longestPalindromeSubseqFromLeft(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s == s[::-1]: return len(s)
+        n = len(s)
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+        for i in range(n): dp[i][i] = 1
+        for size in range(2, n+1):
+            for i in range(n-size+1):
+                j = i + size -1
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1] + 2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        return dp[0][n-1]
         
 if __name__ == '__main__':
     sol = Solution()
-    assert sol.longestPalindromeSubseq('bbbab') == 4
-    assert sol.longestPalindromeSubseq('cbba') == 2
-    assert sol.longestPalindromeSubseq('zzzzzzzzzz') == 10
-    assert sol.longestPalindromeSubseq('abadcaacbddd') == 6
+    assert sol.longestPalindromeSubseqFromRight('bbbab') == 4
+    assert sol.longestPalindromeSubseqFromLeft('bbbab') == 4
+    assert sol.longestPalindromeSubseqFromLeft('cbba') == 2
+    assert sol.longestPalindromeSubseqFromLeft('zzzzzzzzzz') == 10
+    assert sol.longestPalindromeSubseqFromLeft('abadcaacbddd') == 6
