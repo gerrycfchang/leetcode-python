f5f2141e1053e4f74f32a2e19052b65dbb1a5509
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-03-07 13:11:16 +0800

    516. Longest Palindromic Subsequence

--- longest/longest_palindromic_subsequence.py
+++ longest/longest_palindromic_subsequence.py
@@ -37,8 +37,25 @@ class Solution(object):
                     dp[i][j] = max(dp[i+1][j], dp[i][j-1])
         return dp[0][n-1]
         
+    def longestPalindromeSubseqSol(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s == s[::-1]: return len(s)
+        n = len(s)
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = 1
+            for j in range(0,i):
+                if s[i] == s[j]:
+                    dp[j][i] = dp[j+1][i-1] + 2
+                else:
+                    dp[j][i] = max(dp[j+1][i], dp[j][i-1])
+        return dp[0][n-1]
 if __name__ == '__main__':
     sol = Solution()
     assert sol.longestPalindromeSubseq('bbbab') == 4
     assert sol.longestPalindromeSubseq('cbba') == 2
     assert sol.longestPalindromeSubseq('zzzzzzzzzz') == 10
+    assert sol.longestPalindromeSubseqSol('bbbab') == 4
