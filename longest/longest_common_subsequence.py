6135415119e601775d4e81c5325a6d39dd24da76
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-03-08 14:07:39 +0800

    Refactor

--- longest/longest_common_subsequence.py
+++ longest/longest_common_subsequence.py
@@ -2,13 +2,7 @@ class Solution(object):
     def longestCommonSubsequence(self, s1, s2):
         m = len(s1)
         n = len(s2)
-        longestStr = []
-        dp = [[0 for _ in range(n+1)] for _ in range (m+1)]
-        for i in range (m+1):
-            dp[i][0] = 0
-        for j in range (n+1):
-            dp[0][j] = 0
-
+        dp = [[0 for _ in range(n+1)] for _ in range (m+1)] 
         for i in range (1,m+1):
             for j in range(1,n+1):
                 if s1[i-1]==s2[j-1]:
