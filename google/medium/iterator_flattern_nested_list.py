da93ebf7a8fe36a3186046621dc0bec70bf01793
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-03-09 15:36:59 +0800

    341. Flatten Nested List Iterator

--- google/medium/iterator_flattern_nested_list.py
+++ google/medium/iterator_flattern_nested_list.py
@@ -1,7 +1,11 @@
 """
+341. Flatten Nested List Iterator
+
 Given a nested list of integers, implement an iterator to flatten it.
 
-Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+Each element is either an integer, or a list -- 
+
+whose elements may also be integers or other lists.
 """
 
 
