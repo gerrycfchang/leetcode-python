806356317422019fae631a2a9de02c56a1e93bfe
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-03-12 09:16:00 +0800

    796. Rotate String

---
+++ easy/rotate_string.py
@@ -0,0 +1,30 @@
+# 796. Rotate String
+#
+# We are given two strings, A and B.
+# 
+# A shift on A consists of taking string A and moving the leftmost character to the rightmost position. 
+# For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. 
+# Return True if and only if A can become B after some number of shifts on A.
+# 
+# Example 1:
+# Input: A = 'abcde', B = 'cdeab'
+# Output: true
+# 
+# Example 2:
+# Input: A = 'abcde', B = 'abced'
+# Output: false# 
+
+class Solution(object):
+    def rotateString(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: bool
+        """
+        return True if (A+A).find(B) >= 0 else False
+        
+
+if __name__ == '__main__':
+    sol = Solution()
+    assert sol.rotateString('abcde', 'cdeab') == True
+    assert sol.rotateString('abcde', 'abced') == False
