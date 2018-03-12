33e839048ddd83aaeb978499eb2e3e7152fcb742
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-03-12 15:40:08 +0800

    Add 217. Contains Duplicate solution

--- easy/contains_duplicate.py
+++ easy/contains_duplicate.py
@@ -1,4 +1,6 @@
 """
+217. Contains Duplicate
+
 Given an array of integers, find if the array contains any duplicates.
 
 Your function should return true if any value appears at least twice in the array,
@@ -7,7 +9,7 @@ and it should return false if every element is distinct.
 
 """
 
-
+import collections
 class Solution(object):
     def containsDuplicate(self, nums):
         """
@@ -24,9 +26,16 @@ class Solution(object):
 
         return len(table) != len(nums)
 
+    def containsDuplicateSol(self, nums):
+        c = collections.Counter()
+        for num in nums:
+            c[num] += 1
+        return len(c) != len(nums)
+
 if __name__ == '__main__':
     sol = Solution()
 
     assert (sol.containsDuplicate([]) == False)
     assert (sol.containsDuplicate([2, 2, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9]) == True)
+    assert (sol.containsDuplicateSol([2, 2, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9]) == True)
     assert (sol.containsDuplicate([1, 2, 3, 4, 5, 6, 7]) == False)
