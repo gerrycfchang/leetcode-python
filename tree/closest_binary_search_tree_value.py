78985e0423b80835cd73f5504b4d82c4bfeff541
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-03-13 17:22:10 +0800

    Move 270. Closest Binary Search Tree Value tests

--- tree/closest_binary_search_tree_value.py
+++ tree/closest_binary_search_tree_value.py
@@ -1,4 +1,6 @@
 """
+270. Closest Binary Search Tree Value
+
 Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
 
 Note:
@@ -7,6 +9,11 @@ Given target value is a floating point.
 You are guaranteed to have only one unique value in the BST that is closest to the target.
 
 """
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
     def closestValue(self, root, target):
@@ -15,17 +22,63 @@ class Solution(object):
         return self.findValue(root, target)
 
     def findValue(self, node, target):
-        diff = abs(node.value - target)
+        diff = abs(node.val - target)
         if node.left is None and node.right is None:
-            return node.value
+            return node.val
 
-
-        if target > node.value and node.right:
+        if target > node.val and node.right:
             value = self.findValue(node.right, target)
-        if target < node.value and node.left:
+        if target < node.val and node.left:
             value = self.findValue(node.left, target)
 
         if abs(value - target) < diff:
             return value
         else:
-            return node.value
+            return node.val
+
+if __name__ == '__main__':
+    sol = Solution()
+    ### Test case 1
+    """
+
+            19
+           / \
+          7  20
+         /  \
+        5   18
+    """
+    root = TreeNode(19)
+    node1 = TreeNode(7)
+    node2 = TreeNode(20)
+    node3 = TreeNode(5)
+    node4 = TreeNode(18)
+
+    root.left = node1
+    root.right = node2
+    node1.left = node3
+    node1.right = node4
+    sol = Solution()
+    result = sol.closestValue(root, 8)
+    assert (result == 7)
+
+    """
+
+            35
+           / \
+          7  100
+         /  \
+        5   18
+    """
+    root = TreeNode(35)
+    node1 = TreeNode(7)
+    node2 = TreeNode(100)
+    node3 = TreeNode(5)
+    node4 = TreeNode(18)
+
+    root.left = node1
+    root.right = node2
+    node1.left = node3
+    node1.right = node4
+    sol = Solution()
+    result = sol.closestValue(root, 97)
+    assert (result == 100)
