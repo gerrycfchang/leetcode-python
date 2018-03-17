ffaec9989261e1d8b3724a4d004e23b147ab6a04
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-03-17 22:41:37 +0800

    453. Minimum Moves to Equal Array Elements

---
+++ easy/min_move_to_equal_array_elements.py
@@ -0,0 +1,34 @@
+# 453. Minimum Moves to Equal Array Elements
+# 
+# Given a non-empty integer array of size n, find the minimum number of moves required to 
+# make all array elements equal, where a move is incrementing n - 1 elements by 1.
+# 
+# Example:
+# 
+# Input:
+# [1,2,3]
+# 
+# Output:
+# 3
+# 
+# Explanation:
+# Only three moves are needed (remember each move increments two elements):
+# 
+# [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+
+class Solution(object):
+    def minMoves(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total = 0
+        nums.sort()
+        for i in range(len(nums)-1, 0, -1):
+            total += nums[i] - nums[0]
+        return total
+        
+
+if __name__ == '__main__':
+    sol = Solution()
+    assert sol.minMoves([1, 2, 3]) == 3
