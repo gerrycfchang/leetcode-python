483811def2483d432771fa34410976c7a6739918
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-03-24 07:53:54 +0800

    100. Same Tree

---
+++ tree/same_tree.py
@@ -0,0 +1,77 @@
+# 100. Same Tree
+# 
+# Given two binary trees, write a function to check if they are the same or not.
+# 
+# Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+# 
+# 
+# Example 1:
+# 
+# Input:     1         1
+#           / \       / \
+#          2   3     2   3
+# 
+#         [1,2,3],   [1,2,3]
+# 
+# Output: true
+# Example 2:
+# 
+# Input:     1         1
+#           /           \
+#          2             2
+# 
+#         [1,2],     [1,null,2]
+# 
+# Output: false
+# Example 3:
+# 
+# Input:     1         1
+#           / \       / \
+#          2   1     1   2
+# 
+#         [1,2,1],   [1,1,2]
+# 
+# Output: false
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p or  not q: return p is q
+        return (p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+
+
+if __name__ == '__main__':
+    sol = Solution()
+
+    ### Test case 1
+    """
+    Given binary tree [3,9,20,null,null,15,7],
+
+           1
+         /   \
+        2     3 
+    """
+    root1  = TreeNode(1)
+    node11 = TreeNode(2)
+    node12 = TreeNode(3)
+    root1.left = node11
+    root1.right = node12
+
+    root2  = TreeNode(1)
+    node21 = TreeNode(2)
+    node22 = TreeNode(3)
+    root2.left = node21
+    root2.right = node22
+    
+    assert sol.isSameTree(root1, root2) == True  
