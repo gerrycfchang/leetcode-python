5aa7a34e7a0bc32dec41b61521994e92ef378078
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-03-29 14:49:37 +0800

    58. Length of Last Word

---
+++ easy/length_of_last_word.py
@@ -0,0 +1,29 @@
+# 58. Length of Last Word
+# 
+# Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
+# return the length of last word in the string.
+# 
+# If the last word does not exist, return 0.
+# 
+# Note: A word is defined as a character sequence consists of non-space characters only.
+# 
+# Example:
+# 
+# Input: "Hello World"
+# Output: 5
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        slist = s.split()
+        return len(slist[-1]) if len(slist) > 0 else 0
+        
+
+if __name__ == '__main__':
+    sol = Solution()
+    assert sol.lengthOfLastWord('') == 0
+    assert sol.lengthOfLastWord(' ') == 0
+    assert sol.lengthOfLastWord('hello world') == 5
