5626875413221c83ebbc8ca47c720affc62ef912
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-03-31 08:31:10 +0800

    Fix 258. Add Digits python3 syntax

--- easy/add_digits.py
+++ easy/add_digits.py
@@ -19,7 +19,7 @@ class Solution(object):
             temp = 0
             while num > 0:
                 temp += num % 10
-                num /=  10
+                num //=  10
             num = temp
         return num
         
