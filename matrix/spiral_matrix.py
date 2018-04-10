6913a6d566ac53c6972320d16b23d11a1c35911f
gerrycfchang <alfie.gerrycheung@gmail.com>
2018-04-11 07:01:48 +0800

    Refactor 54. Spiral Matrix

--- matrix/spiral_matrix.py
+++ matrix/spiral_matrix.py
@@ -22,19 +22,17 @@ class Solution(object):
         if len(matrix) == 0: return []
         res, dirs, m, n = [], [[0, 1],[1, 0],[0, -1],[-1, 0]], len(matrix), len(matrix[0])
         visited = [[False for _ in range(n)] for _ in range(m)]
-        i, j = 0, 0
+        i, j, idx = 0, 0, 0
         while len(res) < m*n:
-            for _dir in dirs:
-                while i >= 0 and j >= 0 and i < m and j < n and not visited[i][j]:
-                    visited[i][j] = True
-                    res.append(matrix[i][j])
-                    i += _dir[0]
-                    j += _dir[1]
-                if len(res) == m*n: break
-                i -= _dir[0]
-                j -= _dir[1]
-                visited[i][j] = False
-                res.pop()
+            visited[i][j] = True
+            res.append(matrix[i][j])
+            tmpi = i + dirs[idx][0]
+            tmpj = j + dirs[idx][1]        
+            if tmpi >= 0 and tmpj >= 0 and tmpi < m and tmpj < n and not visited[tmpi][tmpj]:
+                i, j = tmpi, tmpj 
+            else:
+                idx = (idx + 1) % 4
+                i, j = dirs[idx][0] + i, dirs[idx][1] + j
         return res
 
 
